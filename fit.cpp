20485f5a28fd5d161607353507a04585127046e3
simonecig <simonecigagna@gmail.com>
2020-11-11 04:14:36 +0100

    new constructor

--- fit.cpp
+++ fit.cpp
@@ -1,8 +1,14 @@
 #include "fit.h"
 
 
-fit::fit(double* x_, double* y_, double *err_y_, int n):
+fit::fit(const double* x_, const double* y_, const double *err_y_, const int n):
     x(x_), y(y_), err_y(err_y_), n(n) {
+    /*arguments:
+     * - array with x data
+     * - array with y data
+     * - array with y errors
+     * - number of elements in each array
+     * NOTE: results will NOT be printed*/
     setSums();
     setDelta();
     setPar_a();
@@ -14,17 +20,67 @@ fit::fit(double* x_, double* y_, double *err_y_, int n):
     setTstud();
 }
 
-fit::fit(double* x_, double* y_, double *err_y, int n, char printRes):
+fit::fit(const double* x_, const double* y_, const double *err_y, const int n, const char printRes):
     fit(x_, y_, err_y, n){
+    /*arguments:
+     * - array with x data
+     * - array with y data
+     * - array with y errorsi
+     * - number of elements in each array
+     * - char 'F' or 'S' for printing the results on a 'F'ile or on the 'S'creen
+     * NOTE: printing on screen will also produce a Graph and will therefore need
+     * to be launched from Root*/
     if(printRes == 'F')
         printOnFile();
     else if(printRes == 'S')
         printOnScreen();
 }
 
-fit::fit(double *x_, double* y_, int n){
-    //todo
+fit::fit(const double *x_, const double* y_, const int n):
+    x(x_), y(y_), n(n){
+    /*arguments:
+     * - array with x data
+     * - array with y data
+     * - number of elements in each array
+     * NOTE: this fit is NOT weighted and will NOT print result*/
+    err_y = new double[n];
+    for(int i = 0; i < n; i++){
+       err_y[i] = 1.;
+    }
+    setSums();
+    setDelta();
+    setPar_a();
+    setErrPost();
+    setR();
+    setTstud();
 }
+
+fit::fit(const string fname, const int numCol){
+    /*arguments:
+     * - file name
+     * - number of columns stored in the file:
+     *   accepted values are either 3 (x, y, errY) or
+     *   4 (x, y , errX, errY)*/
+    ifstream f(fname);
+    double i = 0;
+    vector<double> x_, y_, errX_, errY_;
+    while(f>>i){
+        x_.push_back(i);
+        f >> i;
+        y_.push_back(i);
+        f >> i;
+        if(numCol == 4){
+            errX_.push_back(i);
+            f >> i;
+        }
+        else if(numCol == 3)
+            errX_.push_back(0);
+        errY_.push_back(i);
+    }
+    fit(&x_[0], &y_[0], &errY_[0], x_.size());
+    //TODO: add second fit with xerrors projected onto the y
+}
+
 void fit::setSums(){
     for(int i = 0; i < n; i ++){
         sumX += x[i];
@@ -102,8 +158,23 @@ void fit::printOnScreen(){
     cout << "sigmaP\t" << err_post << endl;
     cout << "correl\t" << r << endl;
     cout << "t-Stud\t" << tStud << endl;
+    cout << "ChiSqu\t" << chisq << endl;
 }
 
 void fit::printOnFile(){
-    //todo
+    ofstream f("fitResult.txt");
+    f << "Num of entries: " << n << endl;
+    f << "Par a\t" << par_a << "\t +/-" << err_a << endl;
+    f << "Par b\t" << par_b << "\t +/-" << err_b << endl;
+    f << "sigmaP\t" << err_post << endl;
+    f << "correl\t" << r << endl;
+    f << "t-Stud\t" << tStud << endl;
+    f << "ChiSqu\t" << chisq << endl;
+}
+
+void fit::setChisq(){
+    double temp = 0;
+    for(int i = 0; i < n; i++)
+        temp += pow(y[i] - par_b*x[i] - par_a,2)/pow(err_y[i],2);
+    chisq = temp;
 }
--- fit.h
+++ fit.h
@@ -1,36 +1,62 @@
+/*
+** Created by Simone Cigagna
+**
+** This class is for linear fits only.
+** See constructors for a detailed explenation on how to use this.
+**
+*/
+
 #ifndef Fit_h
 #define Fit_h
 
 #include <iostream>
 #include <math.h>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class fit{
-public:
-    fit(double* x_, double* y_, double *err_y_, int n);
-    fit(double* x_, double* y_, int n);
-    fit(double* x_, double* y_, double* err_y_, int n, char printRes);
-    double* getX() const {return x;};
-    double* getY() const {return y;};
-    double* getErrY() const {return err_y;};
-    double getPar_a() const {return par_a;};
-    double getPar_b() const {return par_b;};
-private:
-    double sumX, sumY, sumXsq, sumYsq, sumXY;
-    double delta, par_a, par_b, err_a, err_b;
-    double *x,*y, *err_y;
-    int n;
-    double r, err_post, tStud;
-    void setSums();
-    void setDelta();
-    void setPar_a(); void setPar_b(); void setErrParA(); void setErrParB();
-    void setR(); void setErrPost(); void setTstud();
-    double getGeneralSetAverage(double* data);
-    double getGeneralSetError(double* data);
-    void printOnFile();
-    void printOnScreen();
+    public:
+        /* CONSTRUCTORS */
+        fit(const double* x_, const double* y_, const double *err_y_, const int n);
+        fit(const double* x_, const double* y_, const int n);
+        fit(const double* x_, const double* y_, const double* err_y_, const int n, const char printRes);
+        fit(const string fname, const int numCol);
+        /* GET */
+        const double* getX() const {return x;};
+        const double* getY() const {return y;};
+        const double* getErrY() const {return err_y;};
+        double getPar_a() const {return par_a;};
+        double getPar_b() const {return par_b;};
+        // missing stuff
+        // missing destructor
+
+    private:
+        /* VARIABLES */
+        //stuff to make the code shorter
+        double sumX, sumY, sumXsq, sumYsq, sumXY;
+        //fit results
+        double delta, par_a, par_b, err_a, err_b;
+        double r, err_post, tStud, chisq;
+        //data
+        double *x,*y;
+        double *err_y;
+        int n;
+
+        /* FUNCTIONS */
+        //set fit results
+        void setSums();
+        void setDelta();
+        void setPar_a(); void setPar_b(); void setErrParA(); void setErrParB();
+        void setR(); void setErrPost(); void setTstud();
+        void setChisq();
+        double getGeneralSetAverage(double* data);
+        double getGeneralSetError(double* data);
+        //print results
+        void printOnFile();
+        void printOnScreen();
 };
 
 #endif
